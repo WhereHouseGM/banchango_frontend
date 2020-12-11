4309a2d1508dd67426f561209781b66f637795a4
sangwoo-98 <robbyra@gmail.com>
2020-12-11 15:51:46 +0900

    Warehouse Information almost done..

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -34,7 +34,9 @@ const Admin = ({ handleLoginInput, handleLoginSubmit, refreshUser }) => (
       <form>
         <div id="infoWrapper1">
           <span>
-            <span>이메일 </span>
+            <span>
+              이메일<span style={{ color: 'red' }}>*</span>&nbsp;
+            </span>
             <input
               onChange={handleLoginInput}
               type="text"
@@ -44,7 +46,9 @@ const Admin = ({ handleLoginInput, handleLoginSubmit, refreshUser }) => (
               required
             />
             &nbsp;&nbsp;&nbsp;
-            <span>비밀번호 </span>
+            <span>
+              비밀번호<span style={{ color: 'red' }}>*</span>{' '}
+            </span>
             <input
               onChange={handleLoginInput}
               type="password"
@@ -84,6 +88,243 @@ const Admin = ({ handleLoginInput, handleLoginSubmit, refreshUser }) => (
     >
       창고주 로그아웃 하기
     </span>
+    <br />
+    <br />
+    <div style={{ border: '2px solid black' }}>
+      <div>2. 창고 정보 등록</div>
+      <br />
+      <form>
+        <div id="infoWrapper2">
+          <span>
+            <span>
+              창고명<span style={{ color: 'red' }}>*</span>
+            </span>
+            &nbsp;
+            <input type="text" name="name" placeholder="창고명" id="name" />
+            &nbsp;&nbsp;&nbsp;
+            <span>
+              면적(평 단위)<span style={{ color: 'red' }}>*</span>
+            </span>
+            &nbsp;
+            <input
+              type="text"
+              name="area"
+              placeholder="숫자만 입력"
+              id="area"
+            />
+          </span>
+          <br />
+          <br />
+          <span>
+            <span>
+              주소(도로명)<span style={{ color: 'red' }}>*</span>
+            </span>
+            &nbsp;
+            <input
+              type="text"
+              name="address"
+              placeholder="XX시 XX구 XX동"
+              id="address"
+            />
+            &nbsp;&nbsp;&nbsp;
+            <span>
+              상세주소<span style={{ color: 'red' }}>*</span>
+            </span>
+            &nbsp;
+            <input
+              type="text"
+              name="addressDetail"
+              placeholder="XX로 XX"
+              id="addressDetail"
+            />
+          </span>
+          <br />
+          <br />
+          <span>
+            창고 소개(간단 설명, 띄어쓰기 포함 최대 400자)
+            <span style={{ color: 'red' }}>*</span>
+          </span>
+          &nbsp;
+          <textarea
+            id="description"
+            name="description"
+            style={{ width: '100%' }}
+          />
+          <br />
+          <br />
+          <span>
+            오픈 시간<span style={{ color: 'red' }}>*</span>
+          </span>
+          &nbsp;
+          <input
+            type="text"
+            name="openAt"
+            id="openAt"
+            placeholder="ex) 09:00 -> 오전 9시"
+          />
+          &nbsp;&nbsp;&nbsp;
+          <span>
+            마감 시간<span style={{ color: 'red' }}>*</span>
+          </span>
+          &nbsp;
+          <input
+            type="text"
+            name="closeAt"
+            id="closeAt"
+            placeholder="ex) 18:30 -> 오후 6시 30분"
+          />
+          <br />
+          <br />
+          <span>
+            창고 사용 가능 시간에 대한 추가 설명
+            <span style={{ color: 'red' }}>*</span>
+          </span>
+          <textarea
+            id="availableTimeDetail"
+            name="availableTimeDetail"
+            style={{ width: '100%' }}
+            placeholder="ex) 물류 센터 사정에 따라 상이할 수 있습니다."
+          />
+          <br />
+          <br />
+          <div>
+            아래는 해당 사항이 존재(가능) 하다면 체크해주세요.
+            <span style={{ color: 'red' }}>*</span>
+          </div>
+          <br />
+          <span style={{ border: '1px solid gray' }}>
+            <input type="checkbox" value="cctvExist" id="cctvExist" />
+            <label for="cctvExist"> CCTV</label>&nbsp;&nbsp;
+            <input type="checkbox" value="doorLockExist" id="doorLockExist" />
+            <label for="doorLockExist"> 도어락</label>&nbsp;&nbsp;
+            <input
+              type="checkbox"
+              value="securityCompanyExist"
+              id="securityCompanyExist"
+            />
+            <label for="securityCompanyExist"> 보안 업체</label>&nbsp;&nbsp;
+            <input type="checkbox" value="workerExist" id="workerExist" />
+            <label for="workerExist">현장 인력 </label>&nbsp;&nbsp;
+            <input type="checkbox" value="canPickup" id="canPickup" />
+            <label for="canPickup">픽업 </label>&nbsp;&nbsp;
+            <input type="checkbox" value="canPark" id="canPark" />
+            <label for="canPark">주차 </label>
+          </span>
+          <br />
+          <br />
+          <span>
+            <span>보안 업체명(있을 시에만 기재)</span>&nbsp;
+            <input
+              type="text"
+              id="securityCompanyName"
+              name="securityCompanyName"
+              placeholder="업체명"
+            />
+            &nbsp;&nbsp;&nbsp;
+            <span>보험사명(있을 시에만 기재)</span>&nbsp;
+            <input
+              type="text"
+              id="insuranceName"
+              name="insuranceName"
+              placeholder="업체명"
+            />
+          </span>
+          <br />
+          <br />
+          <div style={{ border: '1px solid black' }}>
+            <legend>
+              창고 유형(중복 선택 가능)<span style={{ color: 'red' }}>*</span>
+            </legend>
+            <br />
+            <input
+              type="checkbox"
+              id="ROOM_TEMPERATURE"
+              value="ROOM_TEMPERATURE"
+            />
+            <label for="ROOM_TEMPERATURE"> 상온 창고</label>&nbsp;&nbsp;
+            <input
+              type="checkbox"
+              id="LOW_TEMPERATURE"
+              value="LOW_TEMPERATURE"
+            />
+            <label for="LOW_TEMPERATURE"> 저온 창고</label>&nbsp;&nbsp;
+            <input type="checkbox" id="BONDED" value="BONDED" />
+            <label for="BONDED"> 보세 창고</label>&nbsp;&nbsp;
+            <input type="checkbox" id="SAVAGE" value="SAVAGE" />
+            <label for="SAVAGE"> 상온 창고</label>&nbsp;&nbsp;
+            <input type="checkbox" id="HAZARDOUS" value="HAZARDOUS" />
+            <label for="HAZARDOUS"> 야적 창고</label>&nbsp;&nbsp;
+            <input type="checkbox" id="SELF_STORAGE" value="SELF_STORAGE" />
+            <label for="SELF_STORAGE"> 셀프 스토리지</label>&nbsp;&nbsp;
+            <input type="checkbox" id="CONTAINER" value="CONTAINER" />
+            <label for="CONTAINER"> 컨테이너</label>&nbsp;&nbsp;
+          </div>
+          <br />
+          <div style={{ border: '1px solid black' }}>
+            <legend>
+              냉난방 작동 방식
+              <span style={{ color: 'red' }}>
+                (난방, 냉방 둘 다 되면 모두 선택 하세요)*
+              </span>
+              <br />
+              <br />
+              <div>
+                <input type="checkbox" id="HEATING" value="HEATING" />
+                <label for="HEATING"> 난방</label>&nbsp;&nbsp;
+                <input type="checkbox" id="COOLING" value="COOLING" />
+                <label for="COOLING"> 냉방</label>&nbsp;&nbsp;
+                <input type="checkbox" id="BOTH" value="BOTH" />
+                <label for="BOTH"> 모두</label>&nbsp;&nbsp;
+                <input type="checkbox" id="NONE" value="NONE" />
+                <label for="NONE"> 없음</label>&nbsp;&nbsp;
+              </div>
+            </legend>
+          </div>
+          <br />
+          <div style={{ border: '1px solid black' }}>
+            <span>
+              <span>위도 값</span>&nbsp;
+              <input
+                type="text"
+                id="latitude"
+                name="latitude"
+                placeholder="37.XXXXXX"
+              />
+              <span>경도 값</span>&nbsp;
+              <input
+                type="text"
+                id="longitude"
+                name="longitude"
+                placeholder="128.XXXXXX"
+              />
+            </span>
+          </div>
+          <br />
+          <div style={{ border: '1px solid black' }}>
+            <div>창고 종류 (둘 중 하나 선택)</div>
+            <br />
+            <span>
+              <input
+                type="checkbox"
+                className="warehouseType"
+                id="THREEPL"
+                value="THREEPL"
+              />
+              &nbsp;
+              <label for="THREEPL">3PL</label>&nbsp;&nbsp;&nbsp;
+              <input
+                type="checkbox"
+                className="warehouseType"
+                id="FULFILLMEMT"
+                value="FULFULLMENT"
+              />
+              &nbsp;
+              <label for="FULFULLMENT">풀필먼트</label>
+            </span>
+          </div>
+        </div>
+      </form>
+    </div>
   </>
 );
 
