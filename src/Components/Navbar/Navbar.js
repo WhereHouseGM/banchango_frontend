e2f174cf64486f1a795a18853e19b3cc3c4b525b
sangwoo-98 <robbyra@gmail.com>
2020-12-16 13:46:57 +0900

    Navabar renewal done.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import './Navbar.css';
 import styled from 'styled-components';
 import { message } from 'antd';
 
@@ -16,113 +15,245 @@ const NavContainer = styled.div`
   transition: all 0.3s ease;
   z-index: 100;
   backdrop-filter: saturate(200%) blur(10px);
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
-const NavLeftContainer = styled.div`
+const MobileNavContainer = styled.div`
+  display: none;
+  min-width: 360px;
+  @media screen and (max-width: 768px) {
+    display: flex;
+    width: 100%;
+    min-width: 360px;
+    flex-direction: none;
+    padding-left: 20px;
+    top: 0;
+    padding-right: 20px;
+    position: fixed;
+    align-items: flex-start;
+    padding-top: 9px;
+    background-color: rgba(255, 255, 255, 0.8);
+    backdrop-filter: saturate(200%) blur(10px);
+    transition: all 0.3s ease;
+    z-index: 100;
+  }
+`;
+
+const NavItemWrapper = styled.div`
   display: flex;
-  justify-content: flex-start;
+  flex-direction: row;
+  margin: 0;
+  padding: 0px 35px;
+  max-width: 1350px;
+  width: 100%;
+  height: 60px;
+  box-align: center;
   align-items: center;
-  flex: 1;
+  box-flex: 0;
+  flex-grow: 0;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileNavItemWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin: 0px;
+  padding: 0px;
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0px;
+  padding: 0px;
+  box-pack: justify;
+  justify-content: space-between;
+  box-flex: 1;
+  flex-grow: 0;
+  flex-basis: 100%;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const MobileTopWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0;
+  padding: 0;
+  box-flex: 0;
+  flex-grow: 0;
+  flex-basis: 100%;
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const MobileBottomWrapper = styled.div`
+  flex-direction: row;
+  margin: 0px;
+  padding: 0px;
+  box-pack: start;
+  justify-content: flex-start;
+  box-flex: 0;
+  flex-grow: 0;
+  font-size: 16px;
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const MobileTopContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0px 0px 22px;
+  padding: 0px;
+  box-pack: justify;
+  justify-content: space-between;
+  box-align: center;
+  align-items: center;
+  box-flex: 0;
+  flex-grow: 0;
+  flex-basis: 100%;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0px;
+  padding: 0px;
+  box-flex: 0;
+  flex-grow: 0;
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const MobileLogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 12px 0px 0px;
+  padding: 0px;
+  box-flex: 0;
+  flex-grow: 0;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
 `;
 
 const LogoLink = styled(Link)`
   font-family: 'Jua', sans-serif;
   font-size: 22px;
   font-weight: bold;
-  text-decoration: none;
+  line-height: 1.48;
   color: #333;
   &:hover {
     text-decoration: none;
   }
 `;
 
-const LoginButton = styled(Link)`
+const CenterItemContainer = styled.div`
+  display: flex;
+  flex-direction: none;
+  margin: 0px;
+  padding: 0px;
+  box-pack: justify;
+  justify-content: space-between;
+  box-flex: 0;
+  flex-grow: 0;
+  flex-basis: 409px;
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const Item = styled(Link)`
+  height: 30px;
+  font-family: 'Nanum Gothic', sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+  color: #333;
+  line-height: 1.8;
   white-space: nowrap;
-  padding: 8px 20px;
+  letter-spacing: -0.84px;
+  text-decoration: none;
+  &:hover {
+    color: #1c57b0;
+    text-decoration: none;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin-right: 29px;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0;
+  padding: 0;
+  box-flex: 0;
+  flex-grow: 0;
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const MobileButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+  box-flex: 0;
+  flex-grow: 0;
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const Button = styled(Link)`
+  white-space: nowrap;
   border-radius: 4px;
   margin: auto;
   outline: none;
   font-size: 18px;
   color: white;
-  font-weight: bold;
+  padding: 8px 20px;
   background-color: #3090f0;
   transition: 0.2s ease;
+  font-weight: bold;
   cursor: pointer;
   &:hover {
     color: white;
     text-decoration: none;
     background-color: #202cab;
   }
-  @media screen and (max-width: 960px) {
-    display: none;
-  }
-`;
-
-const NavItem = styled.li`
-  display: flex;
-  align-items: center;
-  height: 60px;
-  justify-content: center;
-`;
-
-const NavLink = styled(Link)`
-  color: black;
-  font-family: 'Nanum Gothic', sans-serif;
-  font-weight: bold;
-  text-decoration: none;
-  transition: 0.2s ease;
-  margin-right: 4px;
-  &:hover {
-    color: #1c57b0;
-    text-decoration: none;
-  }
-  @media screen and (max-width: 960px) {
-    text-align: center;
-    width: 100%;
-    height: 100%;
-    padding-top: 15px;
-    margin-top: auto;
-    margin-bottom: auto;
-    color: white;
-    display: table;
-    &:hover {
-      background-color: #1888ff;
-      border-radius: 0;
-    }
-  }
-`;
-
-const NavLinkMobile = styled(Link)`
-  display: none;
-  @media screen and (max-width: 960px) {
-    display: block;
-    text-align: center;
-    padding: 1.5rem;
-    margin: 2rem auto;
-    border-radius: 4px;
-    width: 80%;
-    background: #1888ff;
-    text-decoration: none;
-    color: black;
-    font-size: 1.5rem;
-    &:hover {
-      background: black;
-      text-decoration: none;
-      color: #1888ff;
-      transition: 250ms;
-    }
+  @media screen and (max-width: 768px) {
+    display: flex;
   }
 `;
 
 const Navbar = () => {
-  const [click, setClick] = useState(false);
-
   const [login, setLogin] = useState(JSON.parse(localStorage.getItem('Login')));
 
   const logout = () => {
     setLogin(false);
     localStorage.clear();
-    alert('로그아웃 되었습니다.');
+    alert('정상적으로 로그아웃 되었습니다.');
     window.location.replace('/');
   };
 
@@ -151,64 +282,68 @@ const Navbar = () => {
     );
   };
 
-  const handleClick = () => setClick(!click);
-
-  const closeMobileMenu = () => setClick(false);
-
   const warningMessage = () => {
-    setClick(false);
     message.warn('아직 준비중입니다.');
   };
 
   return (
     <>
       <NavContainer>
-        <NavLeftContainer>
-          <LogoLink to="/">BANCHANGO</LogoLink>
-        </NavLeftContainer>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-        </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <NavItem>
-            <NavLink to="/" onClick={toLoginOrRegisterForm}>
-              창고 등록하기
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/" onClick={warningMessage}>
-              ABOUT US
-            </NavLink>
-          </NavItem>
-          {login ? (
-            <>
-              <NavLinkMobile to="/" onClick={logout}>
-                로그아웃
-              </NavLinkMobile>
-            </>
-          ) : (
-            <>
-              <NavItem>
-                <NavLink to="/signup" onClick={closeMobileMenu}>
-                  회원 가입
-                </NavLink>
-              </NavItem>
-              <li>
-                <NavLinkMobile to="/login" onClick={closeMobileMenu}>
-                  로그인
-                </NavLinkMobile>
-              </li>
-            </>
-          )}
-        </ul>
-        {login ? (
-          <LoginButton to="/" onClick={logout}>
-            로그아웃
-          </LoginButton>
-        ) : (
-          <LoginButton to="/login">로그인</LoginButton>
-        )}
+        <NavItemWrapper>
+          <ItemWrapper>
+            <LogoContainer>
+              <LogoLink to="/">BANCHANGO</LogoLink>
+            </LogoContainer>
+            <CenterItemContainer>
+              <Item to="/" onClick={toLoginOrRegisterForm}>
+                창고 등록하기
+              </Item>
+              <Item to="/" onClick={warningMessage}>
+                ABOUT US
+              </Item>
+              {login ? null : <Item to="/signup">회원 가입하기</Item>}
+              <ButtonContainer>
+                {login ? (
+                  <Button to="/" onClick={logout}>
+                    로그아웃
+                  </Button>
+                ) : (
+                  <Button to="/login">로그인</Button>
+                )}
+              </ButtonContainer>
+            </CenterItemContainer>
+          </ItemWrapper>
+        </NavItemWrapper>
       </NavContainer>
+      <MobileNavContainer>
+        <MobileNavItemWrapper>
+          <MobileTopWrapper>
+            <MobileTopContainer>
+              <MobileLogoContainer>
+                <LogoLink to="/">BANCHANGO</LogoLink>
+              </MobileLogoContainer>
+              <MobileButtonContainer>
+                {login ? (
+                  <Button to="/" onClick={logout}>
+                    로그아웃
+                  </Button>
+                ) : (
+                  <Button to="/login">로그인</Button>
+                )}
+              </MobileButtonContainer>
+            </MobileTopContainer>
+          </MobileTopWrapper>
+          <MobileBottomWrapper>
+            <Item to="/" onClick={toLoginOrRegisterForm}>
+              창고 등록하기
+            </Item>
+            <Item to="/" onClick={warningMessage}>
+              ABOUT US
+            </Item>
+            {login ? null : <Item to="/signup">회원 가입하기</Item>}
+          </MobileBottomWrapper>
+        </MobileNavItemWrapper>
+      </MobileNavContainer>
     </>
   );
 };
