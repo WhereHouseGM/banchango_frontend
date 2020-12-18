9ddbbff1d9847aa46a326fb22a721d14863b245d
sangwoo-98 <robbyra@gmail.com>
2020-12-18 17:59:44 +0900

    Service agreements page done.

---
+++ src/Routes/SiteRule/SiteRulePresenter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+
+const SiteRulePresenter = () => {
+  return (
+    <>
+      <Navbar />
+      <embed
+        src="https://warehouse-documents-bucket.s3.ap-northeast-2.amazonaws.com/Service-Agreements.pdf"
+        type="application/pdf"
+        scrolling="auto"
+        frameBorder="0"
+        height="100%"
+        width="100%"
+        style={{ position: 'absolute', left: 0, top: 0 }}
+      ></embed>
+    </>
+  );
+};
+
+export default SiteRulePresenter;
