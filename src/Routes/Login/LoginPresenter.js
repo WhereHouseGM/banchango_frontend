b7b8b977b4bae7e48128ff986725d167269c5cdf
나상우 <robbyra@gmail.com>
2021-01-14 16:16:45 +0900

    Revert "Temporary commit."
    This reverts commit 6bb15b3d03838130dd27496a8b393fe540b537af.

--- src/Routes/Login/LoginPresenter.js
+++ src/Routes/Login/LoginPresenter.js
@@ -1,31 +1,13 @@
 import Helmet from 'react-helmet';
 import Login from '../../Components/Login/Login';
-import PropTypes from 'prop-types';
 
-const LoginPresenter = ({
-  handleInput,
-  handleSubmit,
-  toSignupPage,
-  handleEmailSend,
-}) => (
+const LoginPresenter = () => (
   <>
     <Helmet>
       <title>반창고 | 로그인</title>
     </Helmet>
-    <Login
-      handleInput={handleInput}
-      handleSubmit={handleSubmit}
-      toSignupPage={toSignupPage}
-      handleEmailSend={handleEmailSend}
-    />
+    <Login />
   </>
 );
-
-LoginPresenter.propTypes = {
-  handleInput: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  toSignupPage: PropTypes.func.isRequired,
-  handleEmailSend: PropTypes.func.isRequired,
-};
 
 export default LoginPresenter;
