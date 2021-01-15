17fa680909ed721fd2010eed239889488f3e3f99
Poxios <62606632+cassfresh@users.noreply.github.com>
2021-01-15 18:41:49 +0900

    푸터 수정함

--- src/Routes/IconSource/index.js
+++ src/Routes/IconSource/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import Footer from '../../Components/Footer';
 import Navbar from '../../Components/Navbar';
+
 const Container = styled.div`
   width: 100%;
   min-height: 90vh;
   display: flex;
   flex-direction: column;
+  padding: 20px;
   align-items: center;
   justify-content: center;
   line-height: 1.8;
 `;
+
 const IconSource = () => {
   return (
     <>
@@ -18,6 +21,221 @@ const IconSource = () => {
       <Container>
         <div
           style={{ fontSize: '20px', fontWeight: 'bold', marginBottom: '20px' }}
+        >
+          메인페이지
+        </div>
+        <br />
+        <div>
+          의류 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/authors/photo3idea-studio"
+            title="photo3idea_studio"
+          >
+            photo3idea_studio
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          화장품 - Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">
+            iconixar
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          악세서리 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/bags_3226230?related_item_id=3225006&term=accessories"
+            title="iconixar"
+          >
+            iconixar
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          잡화 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/toothbrush_1964882?term=toothbrush&page=3&position=87&related_item_id=1964882"
+            title="photo3idea_studio"
+          >
+            photo3idea_studio
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          식품 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/healthy-food_2798524?term=healthy%20food&page=3&position=91&related_item_id=2798524"
+            title="iconixar"
+          >
+            iconixar
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          서적 - Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">
+            iconixar
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+
+        <div>
+          인테리어 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/light-bulb_3565097?related_item_id=3565107&term=light"
+            title="photo3idea_studio"
+          >
+            photo3idea_studio
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          전자기기 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/smartphone_2798025?term=smartphone&page=2&position=33&related_item_id=2798025"
+            title="iconixar"
+          >
+            iconixar
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          스포츠 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/sports_2553660?related_item_id=2553645&term=sports"
+            title="photo3idea_studio"
+          >
+            photo3idea_studio
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          냉동 - Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">
+            Eucalyp
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          냉장 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/refrigerator_1941810?term=refrigerator&page=1&position=21&related_item_id=1941810"
+            title="iconixar"
+          >
+            iconixar
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/refrigerator_1941892?term=refrigerator&page=3&position=9&related_item_id=1941892"
+            title="iconixar"
+          >
+            iconixar
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          대형물품 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/furnitures_2590393?related_item_id=2590470&term=furniture"
+            title="Nhor Phai"
+          >
+            Nhor Phai
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          전문가 - Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          시계 - Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          체크 - Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/authors/pixel-perfect"
+            title="Pixel perfect"
+          >
+            Pixel perfect
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div>
+          딜리버리 - Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+        <div
+          style={{
+            fontSize: '20px',
+            fontWeight: 'bold',
+            marginBottom: '20px',
+            marginTop: '20px',
+          }}
         >
           창고 상세보기 페이지
           <br />
