cedc4e4c4a9a0dcdc471602d2696bfaa4a818ae3
나상우 <robbyra@gmail.com>
2021-01-18 12:09:55 +0900

    Issue #99 4번째 fixed

--- src/Routes/Category/CategoryContainer.js
+++ src/Routes/Category/CategoryContainer.js
@@ -18,7 +18,7 @@ const CategoryContainer = () => {
       })
       .catch(({ response: { status } }) => {
         if (status === 404) {
-          setError('검색 결과가 존재하지 않습니다.');
+          setError('원하시는 창고가 있나요?');
           setLoading(false);
         } else {
           setError('알 수 없는 오류가 발생했습니다.');
@@ -37,10 +37,12 @@ const CategoryContainer = () => {
         <Loading />
       ) : error ? (
         <ErrorPage
-          title={'오류'}
+          title={'검색 결과가 존재하지 않습니다.'}
           message={error}
-          locationToGo={'/'}
-          buttonMessage={'메인 화면으로 이동'}
+          newLocation={
+            'https://www.cognitoforms.com/WhereHouseGM/%EC%9B%90%ED%95%98%EC%8B%9C%EB%8A%94%EC%B0%BD%EA%B3%A0%EB%A5%BC%EC%A0%81%EC%96%B4%EC%A3%BC%EC%84%B8%EC%9A%94'
+          }
+          buttonMessage={'원하는 창고 요청하기'}
         />
       ) : (
         <CategoryPresenter warehouses={results} />
