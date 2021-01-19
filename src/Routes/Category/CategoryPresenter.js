3e12ce7ac601c27d1607f3e8bf2f83607f48c8c6
나상우 <robbyra@gmail.com>
2021-01-19 16:11:07 +0900

    검색 페이지 더 보기 완료

--- src/Routes/Category/CategoryPresenter.js
+++ src/Routes/Category/CategoryPresenter.js
@@ -4,21 +4,34 @@ import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
 import PropTypes from 'prop-types';
 
-const CategoryPresenter = ({ warehouses }) => {
+const CategoryPresenter = () => {
   return (
     <>
       <Helmet>
         <title>반창고 | 창고 목록 보기</title>
       </Helmet>
       <Navbar />
-      <Category warehouses={warehouses} />
+      <Category />
       <Footer />
     </>
   );
 };
 
-CategoryPresenter.propTypes = {
-  warehouses: PropTypes.arrayOf(Object),
-};
+// const CategoryPresenter = ({ warehouses }) => {
+//   return (
+//     <>
+//       <Helmet>
+//         <title>반창고 | 창고 목록 보기</title>
+//       </Helmet>
+//       <Navbar />
+//       <Category warehouses={warehouses} />
+//       <Footer />
+//     </>
+//   );
+// };
+
+// CategoryPresenter.propTypes = {
+//   warehouses: PropTypes.arrayOf(Object),
+// };
 
 export default CategoryPresenter;
