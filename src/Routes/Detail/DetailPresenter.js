78b4681fc8882781f4d80f0896d83fd31451ee36
sangwoo-98 <robbyra@gmail.com>
2021-02-24 12:21:18 +0900

    Show warehouse.name in detail page title

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 const DetailPresenter = ({ warehouse }) => (
   <>
     <Helmet>
-      <title>반창고 | 상세페이지</title>
+      <title>반창고 허브 | {warehouse.name}</title>
     </Helmet>
     <Navbar />
     <Detail warehouse={warehouse} />
